d448d3d5437f547b12897fe09c4b7aff7a28a6ac
connortrue <connor.true.94@gmail.com>
2023-08-13 12:58:35 -0500

    lets try this

---
+++ print_array.h
@@ -0,0 +1,8 @@
+#ifndef PRINT_ARRAY_H
+#define PRINT_ARRAY_H
+
+#include <stdio.h>
+
+void print_array(const int *array, size_t size);
+
+#endif /* PRINT_ARRAY_H */
--- sort.h
+++ sort.h
@@ -1,6 +1,5 @@
 #ifndef SORT_H
 #define SORT_H
 #include <stdio.h>
-void print_array(const int *array, size_t size);
 void bubble_sort(int *array, size_t size);
 #endif /* SORT_H */
