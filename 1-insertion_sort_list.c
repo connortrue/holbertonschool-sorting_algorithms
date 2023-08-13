31c1570688a9a6321a82b00913af1633ea7dfc4a
connortrue <connor.true.94@gmail.com>
2023-08-13 14:37:17 -0500

    let's try this

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -12,7 +12,6 @@ void insertion_sort_list(listint_t **list)
 	listint_t *key;
 	listint_t *temp;
 
-	*curr = (*list)->next;
 	if (list == NULL || *list == NULL || (*list)->next == NULL)
                 return;
 
