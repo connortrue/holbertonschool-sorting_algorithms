990bb6ee5cfa8fe968a565009744bda187cde607
connortrue <connor.true.94@gmail.com>
2023-08-13 16:06:27 -0500

    name change

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -6,7 +6,7 @@ void swap(int* a, int* b) {
 	*b = temp;
 }
 
-int partition(int arr[], int low, int high) {
+int partition(int array[], int low, int high) {
 	int pivot = arr[high];
 	int i = low - 1;
 	int j;
@@ -21,7 +21,7 @@ int partition(int arr[], int low, int high) {
 	return (i + 1);
 }
 
-void quick_sort(int arr[], int low, int high) {
+void quick_sort(int array[], int low, int high) {
 	if (low < high) {
 		int pivot = partition(arr, low, high);
 		quick_sort(arr, low, pivot - 1);
@@ -29,7 +29,7 @@ void quick_sort(int arr[], int low, int high) {
 	}
 }
 
-void print_array(int arr[], unsigned int size) {
+void print_array(int array[], unsigned int size) {
 	unsigned int i;
 	for (i = 0; i < size; i++) {
 		printf("%d", arr[i]);
