772f91c2e6126880888f9dcc3a8d086f926f3339
connortrue <connor.true.94@gmail.com>
2023-08-13 13:12:53 -0500

    ok now lets go

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -31,24 +31,3 @@ void bubble_sort(int *array, size_t size)
 	}
 }
 
-/**
- * print_array - Prints the elements of an array
- *
- * @array: Pointer to the array
- * @size: Size of the array
- */
-void print_array(int *array, size_t size)
-{
-	size_t i;
-
-	for (i = 0; i < size; i++)
-	{
-		printf("%d", array[i]);
-
-		if (i != size - 1)
-			printf(", ");
-	}
-
-	printf("\n");
-}
-
--- print_array.h
+++
@@ -1,8 +0,0 @@
-#ifndef PRINT_ARRAY_H
-#define PRINT_ARRAY_H
-
-#include <stdio.h>
-
-void print_array(int *array, size_t size);
-
-#endif /* PRINT_ARRAY_H */
