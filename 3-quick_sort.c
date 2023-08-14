0f3eff60de8b2c0bed34ccbdd4087686bea4066d
connortrue <connor.true.94@gmail.com>
2023-08-13 20:19:52 -0500

    eroor

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -10,9 +10,9 @@ void swap(int *a, int *b)
 int lomuto_partition(int *array, int low, int high)
 {
 	int pivot = array[high];
-	int i = low - 1;
+	int j, i = low - 1;
 
-	for (int j = low; j <= high - 1; j++)
+	for (j = low; j <= high - 1; j++)
 	{
 		if (array[j] <= pivot)
 		{
