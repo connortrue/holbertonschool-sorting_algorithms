7839b6a4977eb06435458feb34ab23c912c0e09a
connortrue <connor.true.94@gmail.com>
2023-08-14 20:17:32 -0500

    betty

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,16 +1,32 @@
 #include "sort.h"
 #define MAX_STACK_SIZE 100
+/**
+ * swap - Swaps the values of two integers.
+ *
+ * @a: pointer to the first integer
+ * @b: pointer to the second integer
+ *
+ * Return: void
+ */
 void swap(int *a, int *b)
 {
 	int temp = *a;
 	*a = *b;
 	*b = temp;
 }
-
+/**
+ * lomuto_partition - Partitions an array using the Lomuto partition scheme.
+ *
+ * @array: pointer to the array
+ * @low: starting index of the partition
+ * @high: ending index of the partition
+ *
+ * Return: index of the pivot element after partitioning
+ */
 int lomuto_partition(int *array, int low, int high)
 {
 	int pivot = array[high];
-	int j,k = low, i = low - 1;
+	int j, k = low, i = low - 1;
 	int sorted = 1;
 
 	for (k = low; k < high; k++)
@@ -22,7 +38,7 @@ int lomuto_partition(int *array, int low, int high)
 		}
 	}
 	if (sorted)
-		return high;
+		return (high);
 
 	for (j = low; j <= high - 1; j++)
 	{
@@ -38,13 +54,20 @@ int lomuto_partition(int *array, int low, int high)
 
 	return (i + 1);
 }
-
+/**
+ * quick_sort - Sorts an array using the quicksort algorithm.
+ *
+ * @array: pointer to the array
+ * @size: size of the array
+ *
+ * returns: void
+ */
 void quick_sort(int *array, size_t size)
 {
 	int stack[MAX_STACK_SIZE];
 	int top = -1;
 
-	if (array == NULL || size <= 1)
+	if (array == NULL || size <= 1 || size == 0)
 		return;
 
 	stack[++top] = 0;
